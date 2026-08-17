c8e78ff6569b75c266b13a61f46844a06c1efae4
Dustin Ford <dustinford02@gmail.com>
2026-08-17 00:28:51 -0500

    Simplify service worker event listeners
    Refactor service worker to simplify install and activate events.

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,1 @@
-const CACHE='study-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET')return;
- e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(hit=>hit||fetch(e.request).then(r=>{
-  const cp=r.clone(); if(r.ok&&new URL(e.request.url).origin===location.origin)caches.open(CACHE).then(c=>c.put(e.request,cp));
-  return r;}).catch(()=>caches.match('./index.html'))));
-});
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil((async()=>{try{const k=await caches.keys();await Promise.all(k.map(x=>caches.delete(x)))}catch(err){}try{await self.registration.unregister()}catch(err){}try{(await self.clients.matchAll({type:'window'})).forEach(c=>{try{c.navigate(c.url)}catch(err){}})}catch(err){}})()));
